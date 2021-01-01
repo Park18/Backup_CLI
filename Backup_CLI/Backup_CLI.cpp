e1f02697b35eeef8ff98c6db8251bf2c239952f4
Park18 <sowoomi35@gmail.com>
2021-01-01 16:51:44 +0900

    testing code

--- Backup_CLI/Backup_CLI.cpp
+++ Backup_CLI/Backup_CLI.cpp
@@ -10,12 +10,13 @@ namespace bfs = boost::filesystem;
 
 int main(int argc, char* argv[])
 {
-	//bfs::path root(std::string("c:/A"));
-	//bfs::path dest(std::string("d:/backup"));
-
 	//backup::core::sync("c:/A","d:/backup");
-	//backup::core::Sync sync;
-	//sync.execute(root, dest);
+
+	bfs::path root(std::string("c:/A"));
+	bfs::path dest(std::string("d:/backup"));
+
+	backup::core::Sync sync;
+	sync.execute(root, dest);
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
